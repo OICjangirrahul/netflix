731beee26875f6e33024e8660ff3d7e706ef056e
rahuljangir <>
2023-03-02 23:27:06 +0900

    spread op

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import Video from './components/Video';
 import './App.css'
 
 function App() {
+  let obj  = {
+    channel : "Coder Dost",
+    views : "100K",
+    time : "4 year ago",
+    title : "Golang"
+  }
  return(
   <>
   <div className="App">
    <div>Videos</div>
+   <Video {...obj}/>
   <Video channel="Coder Dost" views="10K" time="1 year ago" title="React js"/>
   <Video channel="Coder Dost" views="30K" time="2 year ago" title="JavaScript"/>
   </div>
